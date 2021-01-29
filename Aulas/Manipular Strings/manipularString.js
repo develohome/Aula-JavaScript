7b2aaac313911d0c2ec49acec005a5a2df40237a
develohome <77947418+develohome@users.noreply.github.com>
2021-01-29 16:48:14 -0300

    add LowerCase

--- Aulas/Manipular Strings/manipularString.js
+++ Aulas/Manipular Strings/manipularString.js
@@ -9,3 +9,4 @@ console.log(nome.charCodeAt(0)) // retorna o valor A na tabela ascii
 console.log(nome.slice(1,3))
 console.log(nome.slice(1,(parseInt(nome.length) - 1)))
 console.log(nome.substr(1, 4));
+console.log(nome.toLowerCase);
