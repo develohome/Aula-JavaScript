580f3f953436082f614870aa542f37de82bac223
develohome <77947418+develohome@users.noreply.github.com>
2021-02-03 08:50:57 -0300

    start end

---
+++ Aulas/IncludesEndsWith/endsWith.js
@@ -0,0 +1,6 @@
+// o comando endsWith serve para verificar se a variavel termina com o parametro passado
+// da mesma forma podemos usar o startWith
+let nomes = "Alex";
+console.log(nomes.endsWith('lex'))
+
+console.log(nomes.startsWith('Al'))
---
+++ Aulas/IncludesEndsWith/includes.js
@@ -0,0 +1,5 @@
+// include serve para localizar se existe o nome passado está na lista
+
+const nomes = ["alex", "jorge", "gabi"];
+
+console.log(nomes.includes('alex'))
