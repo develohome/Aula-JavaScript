7224a26c6f2c188e803f2c4a0af5929252be4348
develohome <77947418+develohome@users.noreply.github.com>
2021-02-14 15:08:17 -0300

    ordernar numeros

--- array multi/order.js
+++ array multi/order.js
@@ -1,3 +1,11 @@
-var lista_frutas = ['Maçã', 'Uva', 'Banana', 'Morango']
+// var lista_frutas = ['Maçã', 'Uva', 'Banana', 'Morango']
 
-console.log(lista_frutas.sort())
+// console.log(lista_frutas.sort())
+
+var listaNumero = [12,40,3,7,19,1]
+
+console.log(listaNumero.sort(ordenaNumeros))
+
+function ordenaNumeros(a, b){
+    return a - b
+}
